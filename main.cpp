541ce26724d33786597aa0c36408215386458d5d
Mantas Einoris <147090675+mantas001@users.noreply.github.com>
2024-02-12 21:55:18 +0200

    Antras sync'as
    Čia nuskaitomas studento vardas pavardė ir egzamino balas.

--- main.cpp
+++ main.cpp
@@ -3,12 +3,33 @@
 
 using namespace std;
 
-struct duom{
+struct stud{
         string vard, pav;
         int rez_nd, rez_egz;
 };
 
 int main()
 {
+    int n=0;
+    int m=0;
 
+    stud *grupe = new stud [n];
+
+    cout<<"Kiek studentų yra grupėje? ";
+    cin >> n;
+
+    delete [] grupe;
+
+    grupe = new stud [n];
+
+    for (int i=0; i<n; i++){
+        cout << "Studento vardas ir pavarde: ";
+        cin >> grupe[i].vard >> grupe[i].pav;
+        cout << endl;
+
+        cout << "Studento egzamino rezultatas: ";
+        cin >> grupe[i].rez_egz;
+        cout << endl;
+    }
+    return 0;
 }
